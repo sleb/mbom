d2cd3933858b989119638a687bedeac7ebb7afcb
sleb <scott.g.lebaron@gmail.com>
2019-02-16 00:50:47 -0800

    fix audio file typo

--- src/main/kotlin/handler/FavoriteColorIntentHandler.kt
+++ src/main/kotlin/handler/FavoriteColorIntentHandler.kt
@@ -1,12 +1,10 @@
 package com.scorpapede.mbom.handler
 
 import com.scorpapede.mbom.speech.IS_SWAMP_A_COLOR
-import com.scorpapede.mbom.speech.YEAH_NO
 
 class FavoriteColorIntentHandler : BaseMonsterIntentHandler(
     "FavoriteColorIntent",
     arrayListOf(
-        IS_SWAMP_A_COLOR,
-        YEAH_NO
+        IS_SWAMP_A_COLOR
     )
 )
